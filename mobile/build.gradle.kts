64cc43160235cbe858f4763329be83ca1f05638d
Tuyen <tuyenctbk@gmail.com>
2026-06-23 23:41:47 +0700

    Increment version codes (wear=4, tv=5, mobile=6) to allow clean Google Play Store upload

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.aetherscreen"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
